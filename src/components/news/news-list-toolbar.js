e3b36718d5e686a2e816de564f584f39c0250cdb
Dhliv <david.henao.martinez@correounivalle.edu.co>
2022-06-14 16:26:52 -0500

    onClose event in ModalAlert now reloads page on successful new's register

--- src/components/news/news-list-toolbar.js
+++ src/components/news/news-list-toolbar.js
@@ -1,55 +1,51 @@
 import axios from 'axios'
-import {Box,Button,Typography, Modal} from '@mui/material';
+import { Box, Button, Typography, Modal } from '@mui/material';
 import { useState } from 'react';
- 
-import {NewsRegisterForm} from '../news/news-register-form'
-import {useStyles} from "../modals/modalAlert"
 
+import { NewsRegisterForm } from '../news/news-register-form'
+import { useStyles } from "../modals/modalAlert"
+
+/**
+ * 
+ * @param {{setSuccessfulRegister: function}} props 
+ * @returns 
+ */
 export const NewsListToolbar = (props) => {
-    const [buttonNews, setButtonNews] = useState(false);
-    const styles = useStyles();     
+  const [buttonNews, setButtonNews] = useState(false);
+  const styles = useStyles();
 
-    return(
-        <Box {...props}>
-        <Box
-            sx={{
-            alignItems: 'center',
-            display: 'flex',
-            justifyContent: 'space-between',
-            flexWrap: 'wrap',
-            m: -1
-            }}
-        >
-            <Typography
-            sx={{ m: 1 }}
-            variant="h4"
-            >
-            Noticias
-            </Typography>
-            <Box sx={{ m: 1, gap:'12px', display:'flex'}}>
-                <Button
-                    color="primary"
-                    variant="contained"
-                    onClick={() => setButtonNews(!buttonNews)}
-                >
-                    Añadir noticia
-                </Button>
-                <Button
-                    color="primary"
-                    variant="contained"
-                    onClick={() => console.log("aquí se hace el update")}
-                >
-                    Actualizar noticia
-                </Button>
-            </Box>
+  return (
+    <Box {...props}>
+      <Box
+        sx={{
+          alignItems: 'center', display: 'flex', justifyContent: 'space-between',
+          flexWrap: 'wrap', m: -1
+        }}
+      >
+        <Typography sx={{ m: 1 }} variant="h4">
+          Noticias
+        </Typography>
+
+        <Box sx={{ m: 1, gap: '12px', display: 'flex' }}>
+          <Button color="primary" variant="contained"
+            onClick={() => setButtonNews(!buttonNews)}
+          >
+            Añadir noticia
+          </Button>
+
+          <Button color="primary" variant="contained"
+            onClick={() => console.log("aquí se hace el update")}
+          >
+            Actualizar noticia
+          </Button>
         </Box>
-        {(buttonNews == true) ? 
+      </Box>
+      {(buttonNews == true) ?
         <Modal open={buttonNews} onClose={() => setButtonNews(!buttonNews)}>
-            <div className={styles.modal}>
-                <NewsRegisterForm ></NewsRegisterForm>
-            </div>
-        </Modal>:<></>}
+          <div className={styles.modal}>
+            <NewsRegisterForm setSuccessfulRegister={props.setSuccessfulRegister} />
+          </div>
+        </Modal> : <></>}
     </Box>
-    )
-  };
-  
+  )
+};
--- src/components/news/news-register-form.js
+++ src/components/news/news-register-form.js
@@ -2,12 +2,12 @@ import * as Yup from 'yup';
 import { Box, Card, CardContent, CardHeader, Divider, Grid, TextField, TextareaAutosize } from '@mui/material';
 import LoadingButton from '@mui/lab/LoadingButton';
 import { useFormik } from 'formik';
-import { createNews, eventsTitle, executed} from 'src/utils/newsAxios';
+import { createNews, eventsTitle, executed } from 'src/utils/newsAxios';
 import { useEffect, useState } from 'react';
 import { ModalAlert } from '../modals/modalAlert';
 import { border } from '@mui/system';
 /** 
- * @param {{}} props 
+ * @param {{setSuccessfulRegister: function}} props 
  * @returns React component.
  */
 export const NewsRegisterForm = (props) => {
@@ -26,7 +26,7 @@ export const NewsRegisterForm = (props) => {
       state: 'Activo',
       event_name: 'Evento',
       media_file: null,
-      edition_date:date.getFullYear()+'-'+parseInt(date.getMonth()+1)+"-"+date.getDate() 
+      edition_date: date.getFullYear() + '-' + parseInt(date.getMonth() + 1) + "-" + date.getDate()
 
     },
     validationSchema: Yup.object().shape({
@@ -42,7 +42,7 @@ export const NewsRegisterForm = (props) => {
         .object().required('Porfavor seleccione al menos 1 archivo (jpg,jpeg,mp4,mkv)')
     })
   });
-  
+
 
   useEffect(() => {
     /**
@@ -55,15 +55,15 @@ export const NewsRegisterForm = (props) => {
         setLoading(true);
         if (formik.isValid) {
           await createNews(formik);
-          if(executed === true){
+          if (executed === true) {
             setModal(!modal)
             setLoading(!loading);
-            formik.resetForm()
+            formik.resetForm();
+          }
+          setData(false);
+          setLoading(false)
         }
-        setData(false);
-        setLoading(false)
-      }
-      }catch(error){
+      } catch (error) {
         console.log(error)
         setModalError(true)
         setLoading(false)
@@ -71,12 +71,12 @@ export const NewsRegisterForm = (props) => {
     }
     onSubmit();
   }, [data])
-  
+
   /**
    * This function validates fields
    * @param {} e
    */
-   const markErrors = async (e) => {
+  const markErrors = async (e) => {
     const [resp] = await Promise.all([formik.validateForm]);
 
     for (var i in formik.values) {
@@ -120,7 +120,7 @@ export const NewsRegisterForm = (props) => {
             <Grid item md={6} xs={12} >
               <TextField
                 fullWidth
-                error = {Boolean(formik.touched.event_name && formik.errors.event_name)}
+                error={Boolean(formik.touched.event_name && formik.errors.event_name)}
                 helperText={formik.touched.event_name && formik.errors.event_name}
                 label="Seleccione el evento"
                 name="event_name"
@@ -155,33 +155,35 @@ export const NewsRegisterForm = (props) => {
                 variant="outlined"
               />
             </Grid>
-            <Grid item md={12} xs={12} sx={{float:'left', width:'50%'}}>
+            <Grid item md={12} xs={12} sx={{ float: 'left', width: '50%' }}>
               <TextareaAutosize
-                id = "description"
+                id="description"
                 style={formik.errors.description && formik.touched.description ? {
-                  height:'9.3rem', 
-                  padding:'0.75rem', 
-                  borderRadius:'0.6rem',
-                  width:'100%', 
-                  maxWidth:'100%', 
+                  height: '9.3rem',
+                  padding: '0.75rem',
+                  borderRadius: '0.6rem',
+                  width: '100%',
+                  maxWidth: '100%',
                   fontFamily: 'Inter',
                   fontStyle: 'normal',
-                  fontWeight:'400',
+                  fontWeight: '400',
                   fontSize: '16px',
                   lineHeight: '24px',
-                  border:'0.8px solid #e76063'
-                } : 
-                { height:'9.3rem', 
-                  padding:'0.75rem', 
-                  border:'0.8px solid #E3E3E3', 
-                  borderRadius:'0.6rem',
-                  width:'100%', 
-                  maxWidth:'100%', 
-                  fontFamily: 'Inter',
-                  fontStyle: 'normal',
-                  fontWeight:'400',
-                  fontSize: '16px',
-                  lineHeight: '24px' }}
+                  border: '0.8px solid #e76063'
+                } :
+                  {
+                    height: '9.3rem',
+                    padding: '0.75rem',
+                    border: '0.8px solid #E3E3E3',
+                    borderRadius: '0.6rem',
+                    width: '100%',
+                    maxWidth: '100%',
+                    fontFamily: 'Inter',
+                    fontStyle: 'normal',
+                    fontWeight: '400',
+                    fontSize: '16px',
+                    lineHeight: '24px'
+                  }}
                 aria-label="Descripcion"
                 name="description"
                 placeholder='Detalles *'
@@ -191,52 +193,55 @@ export const NewsRegisterForm = (props) => {
                 value={formik.values.description}
                 variant="outlined"
               />
-              {formik.errors.description && formik.touched.description ? 
-                <div style={{color:'#e76063', fontSize:'0.75rem'}}>{formik.errors.description}</div>
-              : null }
+              {formik.errors.description && formik.touched.description ?
+                <div style={{ color: '#e76063', fontSize: '0.75rem' }}>{formik.errors.description}</div>
+                : null}
             </Grid>
           </Grid>
-          
+
         </CardContent>
         <Divider />
-        <Box sx={{ display: 'flex', justifyContent: 'flex-end', p: 2, gap:'0.75rem', alignItems:'center' }} >
+        <Box sx={{ display: 'flex', justifyContent: 'flex-end', p: 2, gap: '0.75rem', alignItems: 'center' }} >
           <div>
-            <input 
-              style={{display:'none'}}
+            <input
+              style={{ display: 'none' }}
               id="media_file"
               type="file"
               accept='.png, .jpg, jpeg, .mp4, .mkv'
               name="media_file"
               required
               onChange={(event) => formik.setFieldValue("media_file", event.target.files[0])}
-              >
+            >
             </input>
-            <label htmlFor="media_file" 
-              style={{color:'#5048E5', fontFamily: 'Inter', fontStyle: 'normal',
-                    fontWeight: '600',fontSize: '0.87rem',lineHeight: '1.5rem', cursor:'pointer'}}>
+            <label htmlFor="media_file"
+              style={{
+                color: '#5048E5', fontFamily: 'Inter', fontStyle: 'normal',
+                fontWeight: '600', fontSize: '0.87rem', lineHeight: '1.5rem', cursor: 'pointer'
+              }}>
               Subir archivo
             </label>
           </div>
-          <LoadingButton 
-            loading={loading} 
-            color="primary" 
-            variant="contained" 
-            onClick={(e) => { markErrors(e)}}>
+          <LoadingButton
+            loading={loading}
+            color="primary"
+            variant="contained"
+            onClick={(e) => { markErrors(e) }}>
             Registrar Noticia
           </LoadingButton>
         </Box>
       </Card>
-      {(modal == true) ? <ModalAlert 
-        title={"Noticia registrada"} 
+      {(modal == true) ? <ModalAlert
+        title={"Noticia registrada"}
         message={"La noticia fue registrada exitosamente!"} modalState={modal}
-        setModalState={setModal}/> : null
-      } 
-      {(modalError == true) ? 
-      <ModalAlert 
-        title={"Noticia NO registrada"} 
-        message={"La noticia NO se pudo registrar!"} modalState={modalError}
-        setModalState={setModalError}/> : null
-      } 
+        setSuccessfulRegister={props.setSuccessfulRegister}
+        setModalState={setModal} /> : null
+      }
+      {(modalError == true) ?
+        <ModalAlert
+          title={"Noticia NO registrada"}
+          message={"La noticia NO se pudo registrar!"} modalState={modalError}
+          setModalState={setModalError} /> : null
+      }
     </form>
   );
 }
