a98868b62201dc22e3a1e8cb0cf217efce1ddcb5
Dhliv <david.henao.martinez@correounivalle.edu.co>
2022-06-14 16:32:33 -0500

    News now loads data from localSession

--- src/pages/Noticias/[id]/Ver.jsx
+++ src/pages/Noticias/[id]/Ver.jsx
@@ -1,12 +1,9 @@
 import { Box } from "@mui/system";
 import Head from "next/head";
-import { useRouter } from "next/router";
 import { DashboardLayout } from "src/components/dashboard-layout";
 import ViewNew from "src/components/news/ViewNew";
 
 export default function VerNoticias() {
-  const router = useRouter();
-  const { id } = router.query;
 
   return (
     <>
@@ -16,7 +13,7 @@ export default function VerNoticias() {
         </title>
       </Head>
       <Box component="main" sx={{ flexGrow: 1, py: 4 }}>
-        <ViewNew id={id}></ViewNew>
+        <ViewNew></ViewNew>
       </Box>
     </>
   )
