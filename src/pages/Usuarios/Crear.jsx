a6edd8c1ec58c757cdc239b197f49a5194eff0ef
Dhliv <david.henao.martinez@correounivalle.edu.co>
2022-06-21 16:25:54 -0500

    User now has image field

--- src/pages/Usuarios/Crear.jsx
+++ src/pages/Usuarios/Crear.jsx
@@ -3,44 +3,49 @@ import { DashboardLayout } from '../../components/dashboard-layout';
 import { Box, Container, Grid, Typography } from '@mui/material';
 import { AccountProfile } from 'src/components/account/account-profile';
 import CreateUserForm from 'src/components/forms/CreateUserForm';
+import { useState } from 'react';
+import { createUser } from '../../utils/userAxios';
+import UserUploadImageCard from 'src/components/user/UploadImageCard';
+import { defaultUserIcon } from 'src/utils/defaultImages';
+export default function CrearUsuario(props) {
+  const [userImage, setUserImage] = useState(defaultUserIcon);
+  const [imageChanged, setImageChanged] = useState(false);
 
+  return (
+    <>
+      <Head>
+        <title>
+          Usuarios
+        </title>
+      </Head>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 8
+        }}
+      >
+        <Container maxWidth="lg">
+          <Typography sx={{ mb: 3 }} variant="h4" >
+            Crear Usuario
+          </Typography>
 
-const CrearUsuario = () => (
-  <>
-    <Head>
-      <title>
-        Usuarios
-      </title>
-    </Head>
-    <Box
-      component="main"
-      sx={{
-        flexGrow: 1,
-        py: 8
-      }}
-    >
-      <Container maxWidth="lg">
-        <Typography sx={{ mb: 3 }} variant="h4" >
-          Crear Usuario
-        </Typography>
-
-        <Grid container spacing={3} >
-          <Grid item lg={4} md={6} xs={12} >
-            <AccountProfile />
+          <Grid container spacing={3} >
+            <Grid item lg={4} md={6} xs={12} >
+              <UserUploadImageCard image={userImage} setImage={setUserImage} setImageChanged={setImageChanged} imageChanged={imageChanged} />
+            </Grid>
+            <Grid item lg={8} md={6} xs={12} >
+              <CreateUserForm type='Crear' finalFunction={createUser} userImage={userImage} imageChanged={imageChanged} />
+            </Grid>
           </Grid>
-          <Grid item lg={8} md={6} xs={12} >
-            <CreateUserForm />
-          </Grid>
-        </Grid>
-      </Container>
-    </Box>
-  </>
-);
+        </Container>
+      </Box>
+    </>
+  );
+}
 
 CrearUsuario.getLayout = (page) => (
   <DashboardLayout>
     {page}
   </DashboardLayout>
 );
-
-export default CrearUsuario;
