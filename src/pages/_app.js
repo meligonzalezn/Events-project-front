e32432d4f9b9ce426bcafa18e3a2f49d5bde0cc2
meliwi <melissa.gonzalez@correounivalle.edu.co>
2022-06-24 10:06:15 -0500

    Google maps: working

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import { CacheProvider } from '@emotion/react';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 import { createEmotionCache } from '../utils/create-emotion-cache';
