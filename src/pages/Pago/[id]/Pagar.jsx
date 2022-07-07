7d931f82faced429010abfe2ce653a1f5c2cbdcf
Dhliv <david.henao.martinez@correounivalle.edu.co>
2022-07-07 17:46:51 -0500

    Pay processing implemented in stepper

--- src/pages/Pago/[id]/Pagar.jsx
+++ src/pages/Pago/[id]/Pagar.jsx
@@ -3,6 +3,7 @@ import { Box, Container } from '@mui/system';
 import Head from 'next/head';
 import { useEffect, useState } from 'react';
 import PayUser from 'src/components/forms/PayUser';
+import SecuentialLoader from 'src/components/loaders/SecuentialLoader';
 import PayMethod from 'src/components/pay/PayMethod';
 import PayStepper from 'src/components/pay/PayStepper';
 import PaySteps from 'src/components/pay/PaySteps';
@@ -11,6 +12,15 @@ import { DashboardLayout } from '../../../components/dashboard-layout';
 const DATOS_PERSONALES = 0;
 const METODO_PAGO = 1;
 const PROCESANDO_PAGO = 2;
+
+/**
+ * Implementa todo el proceso de pagos con un stepper que le
+ * indica al usuario en qué paso se encuentra actualmente.
+ * Consta de 3 pasos cada uno invocando un componente diferente:
+ * 1. Datos personales. 2. Método de Pago. 3. Procesando Pago.
+ * @param {*} props 
+ * @returns React component
+ */
 export default function Pagar(props) {
   const [activeStep, setActiveStep] = useState(0);
   const [displayComponent, setDisplayComponent] = useState(<></>);
@@ -28,15 +38,14 @@ export default function Pagar(props) {
   useEffect(() => {
     const handlePageChange = () => {
       if (activeStep == PROCESANDO_PAGO) {
-        setDisplayComponent(<></>)
-        // ! Set fake loader to process pay.
+        setDisplayComponent(<SecuentialLoader upperMessage='Estamos procesando tu pago' lowerMessage='Por favor espera' setValidStep={setValidStep} />)
       } else if (activeStep == METODO_PAGO) {
-        setDisplayComponent(<PayMethod />);
+        setDisplayComponent(<PayMethod setValidStep={setValidStep} validateData={validateData}
+          setValidateData={setValidateData} />);
       } else if (activeStep == DATOS_PERSONALES) {
         setDisplayComponent(<PayUser setValidStep={setValidStep} validateData={validateData}
           setValidateData={setValidateData} />);
       }
-
     }
 
     handlePageChange();
