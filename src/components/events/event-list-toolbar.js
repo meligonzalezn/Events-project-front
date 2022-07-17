e516e1b164444509a7213731524eca97fcbef531
Michelle <michelle.hernandez@correounivalle.edu.co>
2022-07-17 08:53:34 -0500

    update event in progress

--- src/components/events/event-list-toolbar.js
+++ src/components/events/event-list-toolbar.js
@@ -1,55 +1,63 @@
 import {
-    Box,
-    Button,
-    Card,
-    CardContent,
-    TextField,
-    InputAdornment,
-    SvgIcon,
-    Typography
-  } from '@mui/material';
-  import { Download as DownloadIcon } from '../../icons/download';
-  import { Search as SearchIcon } from '../../icons/search';
-  import { Upload as UploadIcon } from '../../icons/upload';
-  import { useRouter } from 'next/router';
+  Box,
+  Button,
+  Card,
+  CardContent,
+  TextField,
+  InputAdornment,
+  SvgIcon,
+  Typography,
+  Modal
+} from "@mui/material";
+import { Download as DownloadIcon } from "../../icons/download";
+import { Search as SearchIcon } from "../../icons/search";
+import { Upload as UploadIcon } from "../../icons/upload";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import {useStyles} from "../modals/modalAlert"
+import {EventsUpdateForm} from "../events/event-update-form"
 
-  
-  export const EventListToolbar = ({searchHandleChange}) => {
-    const router = useRouter();
-    return (
-    <Box >
+export const EventListToolbar = ({isEmployee, searchHandleChange, setSuccessfulRegister }) => {
+  const [updateEvent, setUpdateEvent] = useState(false);
+  const styles = useStyles(); 
+  const router = useRouter();
+
+  const showButtons = () => {
+    isEmployee = true;
+    if(!isEmployee) return (<> </>);
+    return(
+      <Box sx={{ m: 1 }}>
+          <Box sx={{ m: 1, gap: "12px", display: "flex" }}>
+            <Button
+              color="primary"
+              variant="contained"
+              onClick={() => router.push("/Crear_evento")}
+            >
+              Añadir evento
+            </Button>
+            <Button color="primary" variant="contained"
+              onClick = {() => setUpdateEvent(!updateEvent)}>
+              Actualizar evento
+            </Button>
+          </Box>
+        </Box>
+    );
+  }
+  return (
+    <Box>
       <Box
         sx={{
-          alignItems: 'center',
-          display: 'flex',
-          justifyContent: 'space-between',
-          flexWrap: 'wrap',
-          m: -1
+          alignItems: "center",
+          display: "flex",
+          justifyContent: "space-between",
+          flexWrap: "wrap",
+          m: -1,
         }}
       >
-        <Typography
-          sx={{ m: 1 }}
-          variant="h4"
-        >
+        <Typography sx={{ m: 1 }} variant="h4">
           Eventos
         </Typography>
-        <Box sx={{ m: 1 }}>
-        <Box sx={{ m: 1, gap: '12px', display: 'flex' }}>
-        <Button
-          color="primary"
-          variant="contained"
-          onClick={ () => router.push('/Crear_evento')}
-        >
-          Añadir evento
-        </Button>
-        <Button
-          color="primary"
-          variant="contained"
-        >
-          Actualizar evento
-        </Button>
-        </Box>
-      </Box>
+        {showButtons()}
       </Box>
       <Box sx={{ mt: 3 }}>
         <Card>
@@ -60,16 +68,13 @@ import {
                 InputProps={{
                   startAdornment: (
                     <InputAdornment position="start">
-                      <SvgIcon
-                        fontSize="small"
-                        color="action"
-                      >
+                      <SvgIcon fontSize="small" color="action">
                         <SearchIcon />
                       </SvgIcon>
                     </InputAdornment>
-                  )
+                  ),
                 }}
-                onChange= {searchHandleChange}
+                onChange={searchHandleChange}
                 placeholder="Buscar evento"
                 variant="outlined"
               />
@@ -77,5 +82,15 @@ import {
           </CardContent>
         </Card>
       </Box>
+      {updateEvent == true ? (
+        <Modal open={updateEvent} onClose={() => setUpdateEvent(!updateEvent)}>
+          <div className={styles.modal} style={{ width: "700px" }}>
+            <EventsUpdateForm setSuccessfulRegister={setSuccessfulRegister} ></EventsUpdateForm>
+          </div>
+        </Modal>
+      ) : (
+        <></>
+      )}
     </Box>
-  );}
+  );
+};
