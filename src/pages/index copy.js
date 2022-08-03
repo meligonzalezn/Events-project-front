b6e2f992c959174156ca96feb4505d84ccf87542
Min <michelle.hernandez@correounivalle.edu.co>
2022-08-02 22:53:59 -0500

    report events per month in progress

--- src/pages/index copy.js
+++ src/pages/index copy.js
@@ -3,7 +3,7 @@ import { Box, Container, Grid } from '@mui/material';
 import { Budget } from '../components/dashboard/budget';
 import { LatestOrders } from '../components/dashboard/latest-orders';
 import { LatestProducts } from '../components/dashboard/latest-products';
-import { Sales } from '../components/reports/sales';
+import { Sales } from '../components/dashboard/sales';
 import { TasksProgress } from '../components/dashboard/tasks-progress';
 import { TotalCustomers } from '../components/dashboard/total-customers';
 import { TotalProfit } from '../components/dashboard/total-profit';
@@ -14,7 +14,7 @@ const Dashboard = () => (
   <>
     <Head>
       <title>
-        Reportes 
+        Dashboard | Material Kit
       </title>
     </Head>
     <Box
